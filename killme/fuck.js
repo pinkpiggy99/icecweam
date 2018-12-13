81ba4ac9a017b2b56af34a1a46aadff9c19cb4b8
Isa Foti <pinkpiggy4546@gmail.com>
2018-12-13 09:08:57 -0600

    got it?

---
+++ killme/fuck.js
@@ -0,0 +1,36 @@
+
+
+
+// var apple = document.getElementById("apple-div")
+//var apricot = document.getElementById("apricot-div");
+//var moving = false;
+
+//apple.addEventListener("mousedown", initialClick, false);
+//apricot.addEventListener("mousedown", initialClick, false);
+
+
+//function move(e){}
+
+  //var newX = e.clientX - 10;
+//  var newY = e.clientY - 10;
+
+//image.style.left = newX + "px";
+//  image.style.top = newY + "px";
+
+
+}
+
+//function initialClick(e) {
+
+//  if(moving){
+//    document.removeEventListener("mousemove", move);
+//    moving = !moving;
+//    return;
+  }
+
+//  moving = !moving;
+//  image = this;
+
+//  document.addEventListener("mousemove", move, false);
+
+}
---
+++ killme/index.js
@@ -0,0 +1,1 @@
+alert("Welcome to make your own icecream sundae! First you got to find the flavors!");
